48a34912924c311786eb8a3906255cb29ab73017
LazyTechwork <lazytechwork@gmail.com>
2021-12-16 02:38:45 +0300

    Envoy config change

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,11 +2,9 @@
 
 @task('deploy')
 cd /var/www/etu_mailservice
-./vendor/bin/sail stop
 php artisan down --refresh=30
 git pull origin master
 composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
 php artisan migrate --force
-./vendor/bin/sail up -d
 php artisan up
 @endtask
