ff5033d0a323dc0d02d646c2bd206ee4b38ab331
LazyTechwork <lazytechwork@gmail.com>
2021-12-16 17:20:44 +0300

    Added optimization on deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 php artisan down --refresh=30
 git fetch --all
 git reset --hard origin/master
+php artisan clear-compiled
+composer dump-autoload
+php artisan optimize
 composer install --no-ansi --no-dev --prefer-dist --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
 php artisan migrate --force
 php artisan cache:clear
